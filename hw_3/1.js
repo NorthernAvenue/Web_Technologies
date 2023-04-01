6cab099e9cdde58d3a89c18421af2dec9e13af2f
NorthernAvenue <NorthernAvenueAI@gmail.com>
2023-04-01 20:58:44 +0300

    ready

---
+++ hw_3/1.js
@@ -0,0 +1,7 @@
+const celsius = prompt("Введите температуру в градусах Цельсия:");
+const fahrenheit = (9 / 5) * celsius + 32;
+alert(`Цельсий: ${celsius}, Фаренгейт: ${fahrenheit.toFixed(1)}`);
+
+  
+
+
---
+++ hw_3/2.js
@@ -0,0 +1,5 @@
+const userName = prompt("Введит своё имя")
+function greeting(name) {
+    return alert(`Привет ${name} ^_^`)
+}
+greeting(userName)
