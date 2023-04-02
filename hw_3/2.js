f8a5c12977f651dc7d047707870584dabd85f88d
NorthernAvenue <NorthernAvenueAI@gmail.com>
2023-04-02 08:37:58 +0300

    ready

--- hw_3/2.js
+++ hw_3/2.js
@@ -1,5 +1,5 @@
-const userName = prompt("Введит своё имя")
+const userName = prompt("Введит своё имя");
 function greeting(name) {
-    return alert(`Привет ${name} ^_^`)
+    alert(`Привет ${name} ^_^`);
 }
-greeting(userName)
+greeting(userName);
